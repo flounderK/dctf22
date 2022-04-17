67775031307a48dd766944eb240751fb4132f56a
Clif Wolfe <clifwolfe1@gmail.com>
2022-04-17 16:27:14 -0400

    removed unnecessary functions

--- pwn/phonebook/phonebook/chall/solve.py
+++ pwn/phonebook/phonebook/chall/solve.py
@@ -21,8 +21,6 @@ context.terminal = ['gnome-terminal', '-e']
 
 # default libc path for some dists is /usr/lib/libc.so.6
 lib = ELF('libc.so.6') if not args.REMOTE else ELF('libc.so.6')
-# lib.sym['binsh'] = lib.offset_to_vaddr(lib.data.find(b'/bin/sh'))
-# lib.sym['one_gadget'] = argparse_args.one_gadget[0] if argparse_args.one_gadget else 0
 # binary = context.binary = ELF('phonebook')
 binary = context.binary = ELF('phonebook.patched')
 
@@ -48,28 +46,7 @@ def new_proc(start_gdb=False, val=None):
         attach_gdb(p, val)
     return p
 
-def bnot(n, numbits=context.bits):
-    return (1 << numbits) -1 -n
-
-def align(val, align_to):
-    return val & bnot(align_to - 1)
-
-def batch(it, sz):
-    length = len(it)
-    for i in range(0, length, sz):
-        yield it[i:i+sz]
-
 p = new_proc(context.log_level == logging.DEBUG) if not args.REMOTE else remote('51.124.222.205', 13380)
-
-
-class MallocChunk(ctypes.Structure):
-    _fields_ = [("mchunk_prev_size", ctypes.c_size_t),
-                ("mchunk_size", ctypes.c_size_t),
-                ("fd", ctypes.c_void_p),
-                ("bk", ctypes.c_void_p),
-                ("fd_nextsize", ctypes.c_void_p),
-                ("bk_nextsize", ctypes.c_void_p)]
-
 
 # struct person {
 #     uchar phone_number[8];
